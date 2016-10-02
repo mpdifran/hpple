47f98c78f35a26b4be637ab847ab79a16b01132d
Mark DiFranco <mpdifran@gmail.com>
2016-10-02 10:52:14 -0400

    Add framework for Hpple files.

---
+++ Hpple/Hpple/Hpple.h
@@ -0,0 +1,19 @@
+//
+//  Hpple.h
+//  Hpple
+//
+//  Created by Mark DiFranco on 2016-10-02.
+//  Copyright © 2016 MDF Projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hpple.
+FOUNDATION_EXPORT double HppleVersionNumber;
+
+//! Project version string for Hpple.
+FOUNDATION_EXPORT const unsigned char HppleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hpple/PublicHeader.h>
+
+
