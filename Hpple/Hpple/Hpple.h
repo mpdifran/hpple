d5dc5cf550ff4cd91a3cb6dda345f031d1b1e286
Mark DiFranco <mpdifran@gmail.com>
2016-10-02 11:15:49 -0400

    Add imports to public header.

--- Hpple/Hpple/Hpple.h
+++ Hpple/Hpple/Hpple.h
@@ -16,4 +16,6 @@ FOUNDATION_EXPORT const unsigned char HppleVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Hpple/PublicHeader.h>
 
-
+#import "TFHpple.h"
+#import "TFHppleElement.h"
+#import "XPathQuery.h"
